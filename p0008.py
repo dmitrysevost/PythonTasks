e62c5c4e46cded2b9ee12f12e3978be32914cfdd
Dmitry <dsevost@gmail.com>
2021-09-20 15:16:47 +0300

    Update p0008.py

--- p0008.py
+++ p0008.py
@@ -4,19 +4,12 @@ print(x_lines)
 x_lines.reverse()
 f_out = open('file_end.txt', 'w')
 for i in range(len(x_lines)):
-    cur_string = x_lines[i]
-    cur_length_of_string = len(cur_string)
-    cur_out_string = ""
-    for j in range(cur_length_of_string):
-        cur_out_string = cur_out_string + cur_string[cur_length_of_string - j - 1]
-    cur_out_string = cur_out_string
-    f_out.write(cur_out_string)
+    f_out.write(x_lines[i][::-1])
 
 f_out.close()
 f_out = open('file_end.txt', 'r')
 x_lines2 = f_out.readlines()
 print(x_lines2)
-
 
 f_in.close()
 f_out.close()
